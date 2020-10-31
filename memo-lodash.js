8fb4a9d98d5c098f3050d594fa4484cd63d5506d
Tobax <impoissants@gmail.com>
2020-10-31 16:03:54 +0100

    memoized

--- memo-lodash.js
+++ memo-lodash.js
@@ -1,10 +1,18 @@
-var fp = require("lodash/fp");
+const fp = require("lodash/fp");
 
-function addo(a, b) {
+function power(a, b) {
   return Math.pow(a + b, b);
 }
-const add = fp.memoize(addo);
-console.log("1", add(240, 120));
-console.log("2", add(140, 100));
-console.log("3", add(240, 120));
-console.log("4", add(140, 100));
+
+// console.log("1", power(240, 120));
+// console.log("2", power(140, 100));
+// console.log("3", power(240, 120));
+// console.log("4", power(140, 100));
+
+const memoizedPower = fp.memoize(power);
+console.log("1", memoizedpower(240, 120)); //This Call is computed & cached
+console.log("2", memoizedpower(140, 100)); //This Call is computed & cached
+console.log("3", memoizedpower(240, 120)); //The result is returned from cache
+console.log("4", memoizedpower(140, 100)); //The result is returned from cache
+console.log("3", memoizedpower(240, 120)); //The result is returned from cache
+console.log("4", memoizedpower(140, 100)); //The result is returned from cache
